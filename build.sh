3b64ea38995fce6f32ba05350f338ba0dd0cefbd
YakoHeri <yakoirag@gmail.com>
2023-12-20 17:29:10 -0300

    primera version desplegable

---
+++ build.sh
@@ -0,0 +1,9 @@
+source venv/scripts/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+reflex init
+reflex export --frontend-only
+rm -rf public
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate 
